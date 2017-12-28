b0191036150e0ec49e3d9779b3a7396799cfffa5
Brett Hutley <brett@hutley.net>
2017-12-28 13:36:53 +0000

    Take rounding into account

--- bank-csv-to-ledger.py
+++ bank-csv-to-ledger.py
@@ -99,7 +99,7 @@ class Allocation:
             amount_to_use = amount * self.amount
             if self.tax_account is not None:
                 if self.tax_is_percent:
-                    tax = amount_to_use - (amount_to_use / (1.0 + self.tax_rate))
+                    tax = round(amount_to_use - (amount_to_use / (1.0 + self.tax_rate)), 2)
                 else:
                     tax = self.tax_rate
             remainder = amount - tax
